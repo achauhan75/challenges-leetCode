a5de33d6f2ce7f7d0caa8100a54de784e3d079e0
= <aishwarya.chauhan75@gmail.com>
2020-06-19 23:02:29 +0530

    Hindex

---
+++ src/com/challenge/JuneChallenge/HIndex.java
@@ -0,0 +1,18 @@
+package com.challenge.JuneChallenge;
+
+public class HIndex {
+    public int hIndex(int[] citations) {
+        if(citations.length==0) return 0;
+        int left = 0, right = citations.length-1, mid;
+        while(left<=right){
+            mid = (left+right)/2;
+            if(citations[mid]==citations.length-mid)
+                return citations.length-mid;
+            else if(citations[mid]<citations.length-mid)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return citations.length-left;
+    }
+}
